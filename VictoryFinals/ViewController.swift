b6c19249ca9456281d1d2df84a71f8c5d6ab3674
Mark Bautista <markbautista@marks-macbook-air.local>
2018-10-17 20:34:09 +0800

    Initial Commit

---
+++ VictoryFinals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VictoryFinals
+//
+//  Created by Mark Bautista on 17/10/2018.
+//  Copyright © 2018 Mark Bautista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
